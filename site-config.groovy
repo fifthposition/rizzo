2f22353773bef7bf8f3b308fc62223b9456f487f
Benjamin Klein <ben@silver-chalice.com>
2011-09-02 21:25:31 -0500

    minor issues of tab spacing, hardly worthy of a commit message

--- site-config.groovy
+++ site-config.groovy
@@ -1,10 +1,10 @@
 site {
-    domain = "www.replace.with.your.domain.com"
+	domain = "www.replace.with.your.domain.com"
 	base = "" // use if the site should be relative to a top level domain - example: /mypath
-    name = "The Name Of Your Site"
+	name = "The Name Of Your Site"
 }
 
 author {
-	    name = "Your Name"
-        email = "your@email.com"
+	name = "Your Name"
+	email = "your@email.com"
 }
